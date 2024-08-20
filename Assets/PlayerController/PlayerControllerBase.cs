c0c502c6a8ae45787a41ba050b4015ad73d08401
jordantkay21 <jordantkay21@gmail.com>
2024-08-20 19:49:51 -0400

    Configured PlayerControllerBase script
    Base script provides a framework for a player controller enforcing the implementation of movement, camera control, jumping, sprinting, and crouching in any subclass.
    
    It handles common functionality like applying gravity and grounding the player, leaving specific implementations to be defined by subclasses.

--- Assets/PlayerController/PlayerControllerBase.cs
+++ Assets/PlayerController/PlayerControllerBase.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
 
-public class PlayerControllerBase : MonoBehaviour
+public abstract class PlayerControllerBase : MonoBehaviour, IMovable, ICameraControllable, IJumper, ISprinter, ICroucher
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected CharacterController controller;
+    [Tooltip("Stores the character's velocity which is used for gravity and other forces")]
+    protected Vector3 velocity;
+    protected bool isGrounded;
+    [Tooltip("Tracks the player's rotation around the x-axis, used for camera control")]
+    protected float xRotation;
+    protected bool isCrouching;
+    [Tooltip("Stores the player's current speed, which could be adjusted based on sprint/crouch state")]
+    protected float currentSpeed;
+
+
+    protected virtual void Awake()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        ApplyGravity();
     }
+
+    protected void ApplyGravity()
+    {
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0) //Resets the downward velocity if the character is grounded to prevent continous falling
+            velocity.y = -2;
+
+        velocity.y += Physics.gravity.y * Time.deltaTime; //Applies gravity overtime
+        controller.Move(velocity * Time.deltaTime); //Moves the character according to the calculated velocity
+    }
+
+
+    public abstract void Crouch();
+    public abstract void Jump();
+    public abstract void Look(Vector2 input);
+    public abstract void Move(Vector2 input);
+    public abstract void Sprint(bool isSprinting);
 }
