dafd1b2f8362c41271b1e4b5b50db5e5c2b76302
jordantkay21 <jordantkay21@gmail.com>
2024-09-06 18:39:18 -0400

    PlayerModel Configured
    Configured player model to hold basic player stats {health, walk speed, run speed} and hold player states {isCrouching, isJumping, isAttacking, isSprinting)
    
    Also has a constructor to initialize a new player model with default values (values have not been tested).
    
    Contains a method to deplete health stat by the damage parameter.

--- Assets/Layer3_Domain/Module_Player/PlayerModel.cs
+++ Assets/Layer3_Domain/Module_Player/PlayerModel.cs
@@ -1,16 +1,47 @@
-using UnityEngine;
 
-public class PlayerModel : MonoBehaviour
+public class PlayerModel 
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    #region Variables
+    //Player Stats
+    public float health;
+    public float walkSpeed;
+    public float sprintSpeed;
 
-    // Update is called once per frame
-    void Update()
+    //State Management
+    public bool isSprinting;
+    public bool isCrouching;
+    public bool isJumping;
+    public bool isAttacking;
+#endregion
+
+    #region Constructors
+    /// <summary>
+    /// Initilize a new Player with default values
+    /// </summary>
+    public PlayerModel()
     {
-        
+        health = 100f;
+        walkSpeed = 5f;
+        sprintSpeed = 10f;
+        isSprinting = false;
+        isCrouching = false;
+        isJumping = false;
+        isAttacking = false;
     }
+    #endregion
+
+    #region Data Adjustments
+    /// <summary>
+    /// Deplete players health by damage amount
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            //handle player death
+        }
+    }
+    #endregion
 }
