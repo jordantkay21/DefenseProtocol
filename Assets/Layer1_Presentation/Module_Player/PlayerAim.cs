cd4b9a170e764d119b6dec9e16c4fab0ab7ba0a9
jordantkay21 <jordantkay21@gmail.com>
2024-09-07 01:24:07 -0400

    PlayerAim Implemented
    Rotates character based off mainCamera's rotation

---
+++ Assets/Layer1_Presentation/Module_Player/PlayerAim.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    public float turnSpeed = 15;
+    Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.deltaTime);
+    }
+}
--- Assets/Layer4_Infrastructure/Utilities/GameManager.cs
+++ Assets/Layer4_Infrastructure/Utilities/GameManager.cs
@@ -41,12 +41,6 @@ public class GameManager : MonoBehaviour
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
-    }
 
     // Update is called once per frame
     void Update()
